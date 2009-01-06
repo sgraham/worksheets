66261ef6b4b00ef37cb976110561cb6d633229e0
Scott Graham <scott.github@h4ck3r.net>
2009-01-06 12:33:43 -0800

    delete unused file (and test twitter post-hook)

--- packages.lisp
+++
@@ -1,9 +0,0 @@
-(defpackage :learnr
-  (:use :common-lisp
-        :cl-cairo2
-        :cl-colors
-        :it.bese.FiveAM))
-
-(in-package :learnr)
-
-(declaim (optimize (debug 3) (safety 3) (speed 0) (space 0)))
